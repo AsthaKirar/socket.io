7a7732363b758b8aacbf7d1e930884261bf6d7ca
AsthaKirar <asthap602@gmail.com>
2024-11-30 23:08:15 +0530

    commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', client => {
+  client.on('event', data => { /* … */ });
+  client.on('disconnect', () => { /* … */ });
+});
+
+
+// Define a route for the home page
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+// Start the server on port 3000
+server.listen(3000, () => {
+    console.log("Server is running connect socket io");
+});
